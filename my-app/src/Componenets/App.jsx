e70b8298963aa0ccf7aa7df07d073e4783aa47da
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-05-27 11:57:31 -0500

    Adding Image file

--- my-app/src/Componenets/App.jsx
+++ my-app/src/Componenets/App.jsx
@@ -3,18 +3,19 @@ import Navbar from "./Navbar";
 import Contact from "./Contact";
 
 function App() {
-  const [showNavBar, setShowNavBar] = useState(false);
+  // const [showNavBar, setShowNavBar] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowNavBar(true);
-    }, 5000);
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setShowNavBar(true);
+  //   }, 5000);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
     <div>
-      {showNavBar ? <Navbar /> : null}
+      {/* {showNavBar ? <Navbar /> : null} */}
+      <Navbar />
       <Contact />
     </div>
   );
--- my-app/src/Componenets/Navbar.jsx
+++ my-app/src/Componenets/Navbar.jsx
@@ -3,10 +3,12 @@ import React from "react";
 function Navbar() {
   return (
     <div className="navbar">
-      <h4>
-        <a href="#contact">Contact</a>
-      </h4>
-      <h4>About Me</h4>
+      <div>
+        <h4>
+          <a href="#contact">Contact</a>
+        </h4>
+        <h4>About Me</h4>
+      </div>
     </div>
   );
 }
