ce0a48062186d46d2cf30c8d0564a3a47ee2d894
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-05-29 11:52:11 -0500

    Section1 development chanegs

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
 import SectionOne from "./Section1";
 
-import Education from "./Education";
-
 function App() {
   return (
     <div>
       <SectionOne />
-
-      <Education />
     </div>
   );
 }
