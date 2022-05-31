344db55dfdf122a3967930cf40726a0fd87d3733
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-05-31 12:23:15 -0500

    section1 compelete

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import SectionOne from "./Section1";
+import Education from "./Section2";
 
 function App() {
   return (
     <div>
       <SectionOne />
+      <Education />
     </div>
   );
 }
---
+++ my-app/src/Components/Section2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Education() {
+  return (
+    <div id="section2">
+      <p>My Education</p>
+    </div>
+  );
+}
+export default Education;
