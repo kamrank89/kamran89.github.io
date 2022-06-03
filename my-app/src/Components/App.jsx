fb35fb5ba98451f6679b7cd1c506eec9d4a715fe
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-03 11:48:35 -0500

    readme remove

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -1,11 +1,13 @@
 import SectionOne from "./Section1";
 import Education from "./Section2";
+import Skills from "./Section3";
 
 function App() {
   return (
     <div>
       <SectionOne />
       <Education />
+      <Skills />
     </div>
   );
 }
---
+++ my-app/src/Components/Section3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section id="section3">
+      <div className="item1">
+        <h1>test</h1>
+      </div>
+      <div className="item2"></div>
+      <div className="item3"></div>
+    </section>
+  );
+}
+
+export default Skills;
