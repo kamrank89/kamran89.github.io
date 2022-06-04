2b78f7f7595387fc3b6a3e9a0db41e90744c373d
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-04 14:40:01 -0500

    skills

--- my-app/src/Components/Section3.jsx
+++ my-app/src/Components/Section3.jsx
@@ -4,7 +4,7 @@ function Skills() {
   return (
     <section id="section3">
       <div className="item1">
-        <h1>test</h1>
+        <h1>Work In progress</h1>
       </div>
       <div className="item2"></div>
       <div className="item3"></div>
